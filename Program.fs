a97a6f21ebad761f2dadd42ae9963013d952db6c
MasterSharp <marco.delpretehome25@outlook.com>
2025-08-29 12:38:51 +0200

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,37 @@
+﻿open System
+open System.Threading
+open DiscordRPC
+open DiscordRPC.Logging
+
+[<EntryPoint>]
+let main argv =
+    // Timer iniziale (es. 5 secondi)
+    Console.WriteLine("Loading...")
+    Thread.Sleep(5000)
+
+    // Inizializza il client con il tuo ID dell'app Discord
+    let client = new DiscordRpcClient("1401258979748872202")
+
+    // Connessione a Discord
+    client.Initialize()
+
+    // Imposta la Rich Presence
+    client.SetPresence(
+        RichPresence(
+            Details = "Playing Clover Client",
+            State = "In the GUI",
+            Assets = Assets(
+                LargeImageKey = "logo",
+                LargeImageText = "Coded in F# by CodeSharp"
+            )
+        )
+    )
+
+    Console.WriteLine("Clover Client by CodeSharp")
+
+    // Mantieni il programma in esecuzione
+    while true do
+        client.Invoke()
+        Thread.Sleep(1000)
+
+    0 // codice di uscita
